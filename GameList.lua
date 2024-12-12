22ce23e9bc64fc0b693212786f05f787a646624f
nkhieu12926 <mathangsting@gmail.com>
2024-12-12 11:43:14 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -4,4 +4,5 @@ getgenv().Games = {
     [7449423635] = "https://raw.githubusercontent.com/nkhieu12926/Script-Game/refs/heads/main/Blox%20Fruits.lua", -- Blox Fruit Sea 3
     [16732694052] = "https://raw.githubusercontent.com/nkhieu12926/Script-Game/refs/heads/main/Fisch.lua", -- Fisch
     [116605585218149] = "https://raw.githubusercontent.com/nkhieu12926/Script-Game/refs/heads/main/GoFishing.Lua", -- Go Fishing
+    [286090429] = "https://raw.githubusercontent.com/nkhieu12926/Script-Game/refs/heads/main/Arsenal.Lua", -- Arsenal
 }
